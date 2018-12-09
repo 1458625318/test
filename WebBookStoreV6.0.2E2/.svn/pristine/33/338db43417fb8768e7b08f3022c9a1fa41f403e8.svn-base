ef97fe2b7bac2f09ae25455d541d5992f0571ae8
1458625318 <1458625318@qq.com>
2018-12-09 19:50:06 +0800

    del

--- WebBookStoreV6.0.2E2/.svn/pristine/33/338db43417fb8768e7b08f3022c9a1fa41f403e8.svn-base
+++
@@ -1,6 +0,0 @@
-// 添加日历组件
-$(".dialog-panel.to-add-book>.unit>.row>.editable>input[name=publicationDate1]")
-		.datebox({
-			width : 154,
-			height : 24
-		});
--- WebBookStoreV6.0.2E2/src/main/webapp/js/admin/admin.js
+++
@@ -1,7 +0,0 @@
-// 设置日期组件的格式化函数
-$.fn.datebox.defaults.formatter = function(date) {
-	var y = date.getFullYear();
-	var m = date.getMonth() + 1;
-	var d = date.getDate();
-	return y + "-" + m + "-" + d;
-};
